aa6fcef8d2837e49692e785de9cf6a579f4c4480
Zakaria <zakaria.soliman1@gmail.com>
2012-07-20 23:52:40 -0400

    added more comments

--- StackSort.h
+++ StackSort.h
@@ -10,16 +10,25 @@ void Sort(MyStack<T>& stack)
   MyStack<T> tempStack;
   while (!stack.IsEmpty())
   {
-	  T element = stack.peek();
+	//we first get the element that we will perform comparisons on
+	  T elem = stack.peek();
 	  stack.pop();
-
-    while (!tempStack.IsEmpty() && tempStack.peek() < element)
+	//when top element in the temporary stack is less than the first element of the
+	//original stack we put it back in the original stack to then push that larger element to the
+	//temporary stack. We want the temporary stack to go form largest to smallest element.
+	
+	//When there's no more element greater than the top of the temporary stack
+	//we incrementally pop the top of the original stack in the temporary stack
+    while (!tempStack.IsEmpty() && tempStack.peek() < elem)
     {
 		stack.push(tempStack.peek());
 		tempStack.pop();
     }
-	tempStack.push(element);
+	tempStack.push(elem);
   }
+  //after the loops the temporary stack is ordered and we can put back the elements in the original stack
+  //the original stack will be ordered with the biggest element on top of the stack and the smallest element
+  //on the bottom of the stack.
   while (!tempStack.IsEmpty())
   {
 	  stack.push(tempStack.peek());
